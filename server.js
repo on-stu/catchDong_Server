addc7759a45a6646eca3394fe852f9ff7fac7afb
on-stu <minsu0523@naver.com>
2021-09-10 14:12:07 +0900

    cors issue

--- server.js
+++ server.js
@@ -9,6 +9,16 @@ const PORT = process.env.PORT || 3001;
 
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
+  next();
+});
 
 app.use(cors());
 io.on("connection", (socket) => {
